579611fd901ef04f88bf4891882089263d48a373
matthewcn56 <65370631+matthewcn56@users.noreply.github.com>
2020-08-02 19:11:32 -0700

    initlalLoginScreen
    This is the start of our login screen, testing how it works on github

---
+++ screens/LoginScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, SafeAreaView, Text } from "react-native";
+export default class LoginScreen extends React.Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>This will be our login page.</Text>
+      </SafeAreaView>
+    );
+  }
+}
